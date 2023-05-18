0c6348128182e0070a8b93c5d67db7a5f06ffdb0
Josh Harris <josh.harris@stfc.ac.uk>
2023-05-18 15:23:02 +0100

    Added file path and name entry boxes to bottom of capture settings tab, relocated run_user button and gave it a more appropriate name

--- src/odin_pico/file_writer.py
+++ src/odin_pico/file_writer.py
@@ -11,20 +11,25 @@ class FileWriter():
     def __init__(self, dev_conf=DeviceConfig(), buffer_manager=BufferManager()):
         self.dev_conf = dev_conf
         self.buffer_manager = buffer_manager
+        self.file_path_origin = "/"
+        self.file_name = ""
+        self.full_file_path = self.file_path_origin + self.file_name
 
     def writeHDF5(self):
-        current_filename = (('/tmp/')+(str(datetime.now())).replace(' ','_')+'.hdf5')
+        if self.file_name == "":
+            self.file_name = ((self.file_path)+(str(datetime.now())).replace(' ','_')+'.hdf5')
+            self.full_file_path = self.file_path_origin + self.file_name
 
         metadata = {
             'active_channels' : self.buffer_manager.active_channels[:]
         }
 
-        with h5py.File(current_filename, 'w') as f:
+        with h5py.File(self.full_file_path, 'w') as f:
             metadata_group = f.create_group('metadata')
             for key, value in metadata.items():
                 metadata_group.attrs[key] = value
            
-            for c,b in zip(self.buffer_manager.active_channels,self.buffer_manager.channel_arrays):
+            for c, b in zip(self.buffer_manager.active_channels, self.buffer_manager.channel_arrays):
                 f.create_dataset(('adc_counts_'+str(c)), data = b)
                 logging.debug(f'Creating dataset: adc_counts_{str(c)} with data : {b}')
 
