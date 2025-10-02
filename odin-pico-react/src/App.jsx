9ebeaa823be6f87ed7cf4a5ea6d85d8603d17e0c
Will Cross <william.cross@stfc.ac.uk>
2025-10-02 14:48:52 +0100

    removed hard coded URL from App.jsx

--- odin-pico-react/src/App.jsx
+++ odin-pico-react/src/App.jsx
@@ -5,7 +5,8 @@ import PicoDashboard from './components/PicoDashboard/PicoDashboard';
 import { useAdapterEndpoint } from 'odin-react';
 
 function App() {
-  const pico_endpoint = useAdapterEndpoint("pico", "http://192.168.0.58:8888", 300);
+  const url = import.meta.env.VITE_ENDPOINT_URL;
+  const pico_endpoint = useAdapterEndpoint("pico", url, 300);
   const [activeTab, setActiveTab] = React.useState('setup');
 
   return (
